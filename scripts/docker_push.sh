76dc9f5853967c7db657ca487828886cbb5cb4e2
Nicola Atzei <natzei@users.noreply.github.com>
2022-09-15 15:28:19 +0200

    CI: add Gitlab actions

--- scripts/docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-echo "Pushing with tag 'latest'"
-docker push balzaclang/balzac-doc:latest
-
-echo "Pushing with tag ${LATEST_VERSION}"
-docker push balzaclang/balzac-doc:${LATEST_VERSION}
