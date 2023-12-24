f6feb6c7b726a11b60505152e5e5edf36d7ffe25
Nicola Atzei <atzeinicola@gmail.com>
2023-12-24 01:26:07 +0100

    Update Copyright and remove View Source link

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'BALZaC'
-copyright = '2021, N. Atzei, M. Bartoletti, T. Cimoli, S. Lande, R. Zunino'
+copyright = 'N. Atzei, M. Bartoletti, T. Cimoli, S. Lande, R. Zunino'
 author = 'N. Atzei, M. Bartoletti, T. Cimoli, S. Lande, R. Zunino'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -177,7 +177,7 @@ html_style = 'css/custom.css'
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
